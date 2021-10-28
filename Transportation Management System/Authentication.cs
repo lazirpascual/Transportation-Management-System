d2af263b5aea0d788183f3a8994d44b976121eaf
Icaro Ryan <rfvb10hu@gmail.com>
2021-10-28 19:47:42 -0400

    Class files added

---
+++ Transportation Management System/Authentication.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using BCrypt;
+
+
+namespace Transportation_Management_System
+{
+    class Authentication
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Authentication auth = new Authentication();
+            auth.ConnectToDB();
+        }
+
+
+        private MySqlConnection conn;
+
+        public bool ConnectToDB()
+        {
+            
+
+
+            
+
+            //conn.Close();
+            Console.WriteLine("Done.");
+
+            return true;
+        }
+
+        private bool CheckUsername(string userName)
+        {
+            bool existent = false;
+
+            Database db = new Database();
+
+            using (MySqlConnection conn = new MySqlConnection(db.GetConnectionStr()))
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                try
+                {
+                    string sql = $"SELECT * FROM Users WHERE Username='{userName}'";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+
+                    // If data is found
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            string DbUsername = rdr[1].ToString();
+                            if (userName == DbUsername)
+                            {
+                                existent = true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            return existent;
+        }
+
+
+        private bool CheckPassword(string password)
+        {
+            // Compare Hased password
+            bool isValid = false;
+
+            Database db = new Database();
+
+            using (MySqlConnection conn = new MySqlConnection(db.GetConnectionStr()))
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                try
+                {
+                    string sql = $"SELECT * FROM Users WHERE Password='{password}'";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+                    
+                    // If data is found
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            string DbPassword = rdr[2].ToString();
+                            if (password == DbPassword)
+                            {
+                                isValid = true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            return isValid;
+        }
+
+
+        private string HashPass(string password)
+        {
+
+        }
+    }
+}
---
+++ Transportation Management System/Database.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Transportation_Management_System
+{
+    class Database
+    {
+        private MySqlConnection Connection { get; set; }
+        private MySqlTransaction Transaction { get; set; }
+        public string Server { get; set; }
+        public string User { get; set; }
+        public string Port { get; set; }
+        public string Password { get; set; }
+        public string DatabaseName { get; set; }
+
+        public Database()
+        {
+            Server = "phtfaw4p6a970uc0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            User = "x4sqrh7d39h1orji;database=qbwrvu2d70g3dyis";
+            Port = "3306";
+            Password = "p6z1zn2nmrv396ke";
+            DatabaseName = "qbwrvu2d70g3dyis";
+        }
+
+        public string GetConnectionStr()
+        {
+            return $"server={Server};user={User};database={DatabaseName};port={Port};password={Password}";
+
+        }
+
+
+    }
+}
