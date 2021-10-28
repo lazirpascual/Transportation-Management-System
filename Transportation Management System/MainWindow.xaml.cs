9649c2e5cd9c6e37592daa6ee4689df73e082622
Icaro Ryan <rfvb10hu@gmail.com>
2021-10-28 19:40:24 -0400

    Check Username Created

--- Transportation Management System/MainWindow.xaml.cs
+++ Transportation Management System/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace Transportation_Management_System
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        //public MainWindow()
+        //{
+            //InitializeComponent();
+
+            
+        //}
     }
 }
