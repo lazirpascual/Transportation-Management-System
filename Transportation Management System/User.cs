2aa3164d3cbaeee66f98dab185eee1502088ba6e
Lazir Pascual <70488281+lazirpascual@users.noreply.github.com>
2021-11-25 18:30:54 -0500

    added the User class with Doxygen comments

--- Transportation Management System/User.cs
+++ Transportation Management System/User.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Transportation_Management_System
 {
+    /// 
+    /// \class User
+    /// 
+    /// \brief The purpose of this class is to represent the role of a user
+    ///
+    /// This class represents the role of a user when they login to the TMS application.
+    /// There are three different types of users that will inherit from User; Buyer, Planner, Admin.
+    ///
+    /// \author <i>Team Blank</i>
+    ///
     class User
     {
+        public string FirstName { get; set; }
+        public int Username { get; set; }
+        public int Password { get; set; }
+        public bool IsActive { get; set; }
+        public string UserType { get; set; }
     }
 }
