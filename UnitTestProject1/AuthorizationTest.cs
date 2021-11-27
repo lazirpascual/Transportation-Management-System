2219fe5c040d83aa94cf4ccc5987a68f53c1b293
Lazir Pascual <70488281+lazirpascual@users.noreply.github.com>
2021-11-27 17:48:13 -0500

    added functional and exception tests for CheckPassowrd

---
+++ UnitTestProject1/AuthorizationTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Transportation_Management_System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AuthorizationTest
+    {
+        [TestMethod]
+        public void CheckUsername_FunctionalTest()
+        {
+            string validUsername = "admin";
+            var auth = new Authentication();
+
+            bool usernameResult = auth.CheckUsername(validUsername);
+            Assert.AreEqual(true, usernameResult);
+        }
+
+        [TestMethod]
+        public void CheckUsername_ExceptionTest()
+        {
+            string invalidUsername = "invalidAdmin";
+            var auth = new Authentication();
+
+            var usernameResult = auth.CheckUsername(invalidUsername);
+            Assert.AreEqual(false, usernameResult);
+        }
+
+        [TestMethod]
+        public void CheckPassword_FunctionalTest()
+        {
+            string validUsername = "admin";
+            string validPassword = "admin";
+            var auth = new Authentication();
+
+            bool passwordResult = auth.CheckUserPassword(validUsername, validPassword);
+            Assert.AreEqual(true, passwordResult);
+        }
+
+        [TestMethod]
+        public void CheckPassword_ExceptionTest()
+        {
+            string validUsername = "admin";
+            string validPassword = "invalidAdmin";
+            var auth = new Authentication();
+
+            bool passwordResult = auth.CheckUserPassword(validUsername, validPassword);
+            Assert.AreEqual(false, passwordResult);
+        }
+    }
+}
---
+++ UnitTestProject1/MarketplaceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Transportation_Management_System;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    public class MarketplaceTest
+    {
+
+    }
+}
