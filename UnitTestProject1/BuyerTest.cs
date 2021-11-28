04a63ad91c94edb543047779bb350e570bbd189e
Lazir Pascual <70488281+lazirpascual@users.noreply.github.com>
2021-11-27 20:40:54 -0500

    added unit tests for Buyer class

---
+++ UnitTestProject1/BuyerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for BuyerTest
+    /// </summary>
+    [TestClass]
+    public class BuyerTest
+    {
+        [TestMethod]
+        public void CreateOrder_FunctionalTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void CreateOrder_ExceptionTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetActiveCustomers_FunctionalTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetActiveCustomers_ExceptionTest()
+        {
+
+        }
+    }
+}
